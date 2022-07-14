78891200fc1e8d1a37a543df75efd8cea5bb5d71
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-07-15 00:18:56 +0100

    Update run_cellranger_multi.sh

--- run_cellranger_multi.sh
+++ run_cellranger_multi.sh
@@ -4,10 +4,13 @@ export PATH=/opt/cellranger-7.0.0:$PATH
 
 ID=RUNID
 CONFIG_CSV=config.csv
+
+JOBMODE=${JOBMODE:-local}
 LOCAL_CORES=${LOCALCORES:-8}
 LOCAL_MEM=${LOCALMEM:-64}
 
 cellranger multi --id=$ID \
                  --csv=$CONFIG_CSV \
+                 --jobmode=$JOBMODE \
                  --localcores=$LOCAL_CORES \
                  --localmem=$LOCAL_MEM
