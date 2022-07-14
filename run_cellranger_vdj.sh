096d91180cfba3571cc25cb67d4fc7b82a3b187d
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-07-14 21:50:32 +0100

    Create run_cellranger_vdj.sh

---
+++ run_cellranger_vdj.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+export PATH=/opt/cellranger-7.0.0:$PATH
+
+ID=RUNID
+PREFIX=SIGAH5
+VDJ_REF=/reference-genomes/refdata-cellranger-vdj-GRCm38-alts-ensembl-7.0.0
+
+FASTQ_PATH=${FASTQPATH:-'./'}
+LOCAL_CORES=${LOCALCORES:-8}
+LOCAL_MEM=${LOCALMEM:-64}
+
+cellranger vdj --id=$RUNID \
+                 --reference=$VDJ_REF \
+                 --fastqs=$FASTQ_PATH \
+                 --sample=$PREFIX \
+                 --localcores=$LOCAL_CORES \
+                 --localmem=$LOCAL_MEM
+                 
+ 
