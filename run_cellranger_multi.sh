457a78bdd943dc1bf6eda738e444f4377bffd35e
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-07-16 09:01:52 +0100

    Update run_cellranger_multi.sh

--- run_cellranger_multi.sh
+++ run_cellranger_multi.sh
@@ -8,9 +8,12 @@ CONFIG_CSV=config.csv
 JOBMODE=${JOBMODE:-local}
 LOCAL_CORES=${LOCALCORES:-8}
 LOCAL_MEM=${LOCALMEM:-64}
+DRYRUN=${DRYRUN:-true}
+
+if [ "$DRYRUN" = true ]; then DRY_RUN="--dry"; else DRY_RUN=""; fi
 
 cellranger multi --id=$ID \
     --csv=$CONFIG_CSV \
     --jobmode=$JOBMODE \
     --localcores=$LOCAL_CORES \
-    --localmem=$LOCAL_MEM
+    --localmem=$LOCAL_MEM $DRY_RUN
