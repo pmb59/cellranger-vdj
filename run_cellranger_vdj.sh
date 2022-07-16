c407f84e1daad4c3687caf9257f8ab9273e8d697
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-07-16 09:02:25 +0100

    Update run_cellranger_vdj.sh

--- run_cellranger_vdj.sh
+++ run_cellranger_vdj.sh
@@ -9,12 +9,15 @@ VDJ_REF=/reference-genomes/refdata-cellranger-vdj-GRCm38-alts-ensembl-7.0.0
 FASTQ_PATH=${FASTQPATH:-'./'}
 LOCAL_CORES=${LOCALCORES:-8}
 LOCAL_MEM=${LOCALMEM:-64}
+DRYRUN=${DRYRUN:-true}
+
+if [ "$DRYRUN" = true ]; then DRY_RUN="--dry"; else DRY_RUN=""; fi
 
 cellranger vdj --id=$ID \
     --reference=$VDJ_REF \
     --fastqs=$FASTQ_PATH \
     --sample=$PREFIX \
     --localcores=$LOCAL_CORES \
-    --localmem=$LOCAL_MEM
+    --localmem=$LOCAL_MEM $DRY_RUN
 
 
